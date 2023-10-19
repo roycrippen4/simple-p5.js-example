3b5fa94f53c45ad7dd1476574122341d9b193c16
roycrippen4 <roycrippen4@gmail.com>
2023-10-18 22:57:01 -0400

    initial commmit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect(100, 100, 200, 200);
+  rect(150, 150, 100, 100);
+  line(100, 100, 150, 150);
+  line(300, 100, 250, 150);
+  line(100, 300, 150, 250);
+  line(300, 300, 250, 250);
+}
